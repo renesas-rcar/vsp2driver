140d143a0e34e80eb07bc6b794b2fe5939b38dba
Sojiro Kusunoki <sojiro.kusunoki.zj@ps.hitachi-solutions.com>
2015-11-10 19:04:05 +0900

    vsp2driver: Move entity route setup function to vsp2_entity.c
    This commit is based on the following commit id in renesas-bsp.git.
    	da3d588eada5cce643cd9777112c39164a6e3c85
    
    Signed-off-by: Sojiro Kusunoki <sojiro.kusunoki.zj@ps.hitachi-solutions.com>

--- vsp2driver/vsp2_entity.h
+++ vsp2driver/vsp2_entity.h
@@ -124,4 +124,6 @@ void vsp2_entity_init_formats(struct v4l2_subdev *subdev,
 bool vsp2_entity_is_streaming(struct vsp2_entity *entity);
 int vsp2_entity_set_streaming(struct vsp2_entity *entity, bool streaming);
 
+void vsp2_entity_route_setup(struct vsp2_entity *source);
+
 #endif /* __VSP2_ENTITY_H__ */
