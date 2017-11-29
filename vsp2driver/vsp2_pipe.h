b683dcad3b21afa5b3f7bc0d834454160646d240
Sojiro Kusunoki <sojiro.kusunoki.zj@ps.hitachi-solutions.com>
2017-11-29 15:06:36 +0900

    vsp2driver: Fix format-info documentation
    This commit is based on the following commit id in renesas-drivers.git.
    tag: renesas-drivers-2017-11-14-v4.14
    	f1450162544f5c4dc801c85bb28f64c0fa6146e7
    
    Signed-off-by: Sojiro Kusunoki <sojiro.kusunoki.zj@ps.hitachi-solutions.com>

--- vsp2driver/vsp2_pipe.h
+++ vsp2driver/vsp2_pipe.h
@@ -73,11 +73,12 @@ struct vsp2_rwpf;
 
 /*
  * struct vsp2_format_info - VSP2 video format description
- * @mbus: media bus format code
  * @fourcc: V4L2 pixel format FCC identifier
+ * @mbus: media bus format code
+ * @hwfmt: VSP2 hardware format
+ * @swap: swap register control
  * @planes: number of planes
  * @bpp: bits per pixel
- * @hwfmt: VSP2 hardware format
  * @swap_yc: the Y and C components are swapped (Y comes before C)
  * @swap_uv: the U and V components are swapped (V comes before U)
  * @hsub: horizontal subsampling factor
